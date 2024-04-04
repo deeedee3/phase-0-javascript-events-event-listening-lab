f9e953afff502222413ce34b26eeef9b659a2517
Deborah <debbiendole@gmail.com>
2024-04-04 08:47:10 +0300

    assignmnent complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+
+function addingEventListener() {
+    document
+      .getElementById("button")
+      .addEventListener("click", alert("I am clicked"));
+  }
+  
+  module.exports = {
+    addingEventListener,
+  }
+  
